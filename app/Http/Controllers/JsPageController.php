7809c7bf2dfd3d324b3b6e95364c0a7a2f6fb635
田畑颯 <chinarra.tabata@gmail.com>
2017-01-29 23:10:48 +0900

    add JsPageController.php

---
+++ app/Http/Controllers/JsPageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class JsPageController
+{
+    public function index()
+    {
+        return view('js.index');
+    }
+}
---
+++ resources/views/js/index.blade.php
@@ -0,0 +1,5 @@
+<h1>JS Test Page</h1>
+<br>
+<br>
+<br>
+<input type="textbox" value="{{ date('Y/m/d') }}">
